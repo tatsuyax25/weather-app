9dc142a0bf65090ac377f8a9e9573558faae7d1f
Miguel Urena <urena_miguel82@outlook.com>
2023-02-14 15:10:25 -0800

    Transforming API Data pt 2

--- weather.js
+++ weather.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone
-
 export function getWeather(lat, lon, timezone) {
     return axios.get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone", { params: {
         latitude: lat,
@@ -12,23 +10,35 @@ export function getWeather(lat, lon, timezone) {
     ).then(({ data}) => {
         return {
             current: parseCurrentWeather(data),
-            daily: parseDailyWeather(data),
-            hourly: parseHourlyWeather(data)
+            // daily: parseDailyWeather(data),
+            // hourly: parseHourlyWeather(data)
         }
     })
 }
 
-function parseCurrentWeather(({ current_weather, daily }) {
-    const { temperature: currentTemp, windspeed: windSpeed, weathercode: iconCode } = current_weather
+function parseCurrentWeather({ current_weather, daily }) {
+    const { 
+        temperature: currentTemp, 
+        windspeed: windSpeed, 
+        weathercode: iconCode 
+    } = current_weather
+
+    const {
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip],
+    } = daily
 
     return {
-        currentTemp,
-        highTemp: ,
-        lowTemp: ,
-        highFeelsLike: ,
-        lowFeelsLike: ,
-        windSpeed,
-        precip: ,
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        highFeelsLike: Math.round(maxFeelsLike),
+        lowFeelsLike: Math.round(minFeelsLike),
+        windSpeed: Math.round(windSpeed),
+        precip: Math.round(precip * 100) / 100,
         iconCode
     }
 }

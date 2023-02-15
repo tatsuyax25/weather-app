1bcd5bbf3b7b52934943086701d71057d63cc976
Miguel Urena <urena_miguel82@outlook.com>
2023-02-15 15:59:33 -0800

    Transforming API Data

--- weather.js
+++ weather.js
@@ -8,11 +8,10 @@ export function getWeather(lat, lon, timezone) {
             },
         }
     ).then(({ data}) => {
-        // return data
         return {
             current: parseCurrentWeather(data),
             daily: parseDailyWeather(data),
-            // hourly: parseHourlyWeather(data)
+            hourly: parseHourlyWeather(data)
         }
     })
 }
@@ -52,4 +51,17 @@ function parseDailyWeather({ daily }) {
             maxTemp: Math.round(daily.temperature_2m_max[index])
         }
     })
+}
+
+function parseHourlyWeather({ hourly, current_weather }) {
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weathercode[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.windspeed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) / 100,
+        }
+    }).filter(({ timestamp }) => timestamp >= current_weather.time * 1000)
 }

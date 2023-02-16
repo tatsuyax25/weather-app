2188dc4711efeaae6e251945837563d45cd0c8a5
Miguel Urena <urena_miguel82@outlook.com>
2023-02-16 15:49:22 -0800

    Rendering the data

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import "./style.css"
 import { getWeather } from "./weather"
 
-getWeather(10, 10, Intl.DateTimeFormat(). resolvedOptions().timeZone).then(data => {
-    console.log(data)
+getWeather(10, 10, Intl.DateTimeFormat(). resolvedOptions().timeZone).then(renderWeather).catch(e => {
+    console.error(e)
+    alert("Error getting weather.")
 })
+
+function renderWeather({ current, daily, hourly }) {
+    renderCurrentWeather(current)
+    renderDailyWeather(daily)
+    renderHourlyWeather(hourly)
+}

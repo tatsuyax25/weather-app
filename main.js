0b46b519503e97d02500c4e3c0f5080147cb7821
Miguel Urena <urena_miguel82@outlook.com>
2023-02-16 16:28:02 -0800

    Rendering the data pt 5

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css"
 import { getWeather } from "./weather"
+import { ICON_MAP } from "./iconMap"
 
 getWeather(10, 10, Intl.DateTimeFormat(). resolvedOptions().timeZone).then(renderWeather).catch(e => {
     console.error(e)
@@ -17,7 +18,13 @@ function setValue(selector, value, { parent = document} = {}) {
     parent.querySelector(`[data-${selector}]`).textContent = value
 }
 
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
 function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
     setValue("current-temp", current.currentTemp)
     setValue("current-high", current.highTemp)
     setValue("current-low", current.lowTemp)
